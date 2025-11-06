7aa1c3f97643eaa5e52e04a7d2b9a086646703e3
Pras-hant-Karki <gymratprashant911@.com>
2025-11-06 10:19:45 +0545

    first

---
+++ bin/assignment_dart.dart
@@ -0,0 +1,5 @@
+import 'package:assignment_dart/assignment_dart.dart' as assignment_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${assignment_dart.calculate()}!');
+}
---
+++ test/assignment_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:assignment_dart/assignment_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
