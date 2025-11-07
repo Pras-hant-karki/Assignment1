e61fff5038ad9b9bfc5d6c4a3791cc636c299b3f
Pras-hant-Karki <gymratprashant911@.com>
2025-11-07 11:57:01 +0545

    assignment code updated

--- lib/bank_account.dart
+++ lib/bank_account.dart
@@ -1,4 +1,28 @@
-abstract class BankAccount{
-    // private fields (encapsulation)
-    
+// abstraction + encapsulation + inheritance + polymorphism
+abstract class BankAccount {
+  // private fields (encapsulation)
+  String _accountNumber;
+  String _accountHolder;
+  double _balance;
+
+  // constructor
+  BankAccount(this._accountNumber, this._accountHolder, this._balance);
+
+  // getters and setters
+  String get getAccountNumber => _accountNumber;
+  String get getAccountHolder => _accountHolder;
+  double get getBalance => _balance;
+
+  set setBalance(double balance) {
+    _balance = balance;
+  }
+
+  // abstract methods (abstraction)
+  void withdraw(double amount);
+  void deposit(double amount);
+
+  // concrete method
+  void displayAccountInfo() {
+    print('Account: $_accountNumber | Holder: $_accountHolder | Balance: $_balance');
+  }
 }
